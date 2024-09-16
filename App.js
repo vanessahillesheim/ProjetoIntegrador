e55b39ddadfb87cd9b7d0df28dfe1dbc584e3d00
Vanessa Hillesheim <162062543+vanessahillesheim@users.noreply.github.com>
2024-09-16 11:13:46 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/Navegacao/StackNavigator"; // Certifique-se de que o caminho está correto
+
  
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <StatusBar />
       <StackNavigator/>
     </NavigationContainer>
+
   );
 }
  
