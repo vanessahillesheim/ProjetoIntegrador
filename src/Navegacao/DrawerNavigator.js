28a002538efe94a6732150f896ad8af2e2d02688
Vanessa Hillesheim <162062543+vanessahillesheim@users.noreply.github.com>
2024-09-23 19:09:06 -0300

    Add files via upload

--- src/Navegacao/DrawerNavigator.js
+++ src/Navegacao/DrawerNavigator.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // DrawerNavigator.js
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -23,3 +24,30 @@ function DrawerNavigator() {
 }
 
 export default DrawerNavigator;
+=======
+// DrawerNavigator.js
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import TabNavigator from './TabNavigator'; // Certifique-se de que o caminho está correto
+import CadastroCorrida from '../Telas/CadastroCorrida';
+import ListaCorridas from '../Telas/ListaCorridas';
+import Calendario from '../Telas/Calendario';
+import Tela1 from '../Telas/Tela1';
+
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      {/* Adiciona o TabNavigator como uma tela do Drawer */}
+      <Drawer.Screen name="Menu" component={TabNavigator} />
+      <Drawer.Screen name="CadastroCorrida" component={CadastroCorrida} />
+      <Drawer.Screen name="ListaCorridas" component={ListaCorridas} />
+      <Drawer.Screen name="Calendario" component={Calendario} />
+      <Drawer.Screen name="Login" component={Tela1} />
+    </Drawer.Navigator>
+  );
+}
+
+export default DrawerNavigator;
+>>>>>>> 7445952cd1ba76e126cb4c5471bc8e60f9c7d606
--- src/Navegacao/TabNavigator.js
+++ src/Navegacao/TabNavigator.js
@@ -1,3 +1,73 @@
+<<<<<<< HEAD
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Menu from '../Telas/Menu';
+import ListaCorridas from '../Telas/ListaCorridas';
+import Calendario from '../Telas/Calendario';
+import DetalhesCorridas from '../Telas/DetalhesCorridas';
+import DiaNoite from '../Telas/DiaNoite';
+import AtualizacaoCorrida from '../Telas/AtualizacaoCorrida';
+
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#12B1F5',
+        tabBarInactiveTintColor: 'gray',
+        tabBarShowLabel: true,
+        tabBarLabelPosition: 'below-icon'
+      }}
+    >
+      <Tab.Screen
+        name="Menu"
+        component={Menu}
+        options={{
+          tabBarIcon: () => <Icon name='bars' size={20} color={'#0038a8'} />,
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="ListaCorridas"
+        component={ListaCorridas}
+        options={{
+          tabBarIcon: () => <Icon name='list' size={20} color={'#0038a8'} />,
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="Calendario"
+        component={Calendario}
+        options={{
+          tabBarIcon: () => <Icon name='info-circle' size={20} color={'#0038a8'} />,
+          headerShown: false
+        }}
+      />
+     {/*  <Tab.Screen
+        name="DetalhesCorridas"
+        component={DetalhesCorridas}
+        options={{
+          tabBarIcon: () => <Icon name='clipboard-list' size={20} color={'#0038a8'} />,
+          headerShown: false
+        }}
+      /> */}
+     
+       <Tab.Screen
+        name="AtualizacaoCorrida"
+        component={AtualizacaoCorrida}
+        options={{
+          tabBarIcon: () => <Icon name='sun' size={20} color={'#0038a8'} />,
+          headerShown: false
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default TabNavigator;
+=======
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome5';
@@ -64,3 +134,4 @@ function TabNavigator() {
 }
 
 export default TabNavigator;
+>>>>>>> 7445952cd1ba76e126cb4c5471bc8e60f9c7d606
