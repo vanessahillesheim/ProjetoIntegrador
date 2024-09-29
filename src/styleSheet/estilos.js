b139b5a3f90e639fca003e4e593db8164b21f948
Vanessa Hillesheim <162062543+vanessahillesheim@users.noreply.github.com>
2024-09-28 21:05:09 -0300

    Add files via upload

--- src/styleSheet/estilos.js
+++ src/styleSheet/estilos.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { StyleSheet } from "react-native";
 
 const estilos = StyleSheet.create({
@@ -34,9 +34,14 @@ const estilos = StyleSheet.create({
         resizeMode: 'contain',
     },
     entrada_texto: {
-        height: 60, width: 270, borderWidth: 1, borderColor: '#12B1F5', marginHorizontal: 10, fontSize: 16, padding: 5,
+        height: 50, width: 270,  marginHorizontal: 10, fontSize: 16, padding: 10,
         borderRadius: 6, marginBottom: 20,
-        textAlign: 'center'
+        textAlign: 'center',
+    },
+    entrada_texto4: {
+        height: 50, width: 270,  marginHorizontal: 10, fontSize: 16, padding: 10,
+        borderRadius: 6, marginBottom: 20,
+        textAlign: 'center', borderWidth: 1, borderColor: '#12B1F5', 
     },
     entrada_texto2: {
         height: 60, width: 270, borderWidth: 1, borderColor: '#12B1F5', marginHorizontal: 10, fontSize: 20, padding: 5,
@@ -244,6 +249,20 @@ const estilos = StyleSheet.create({
         fontWeight: 'bold',
     },
 
+    cabecalhoBotoes: { position: 'absolute',  right: 10 },
+
+    containerAnimacao:{
+        flex: 1, 
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+
+    carregando: {
+        width: 100, 
+        height: 100
+
+    }
 
 
 
@@ -251,201 +270,5 @@ const estilos = StyleSheet.create({
 
 
 });
-=======
-import { StyleSheet } from "react-native";
 
-const estilos = StyleSheet.create({
-
-    fundo: {
-        flex: 1,
-        backgroundColor: 'white'
-
-    },
-
-    cabecalho: {
-        flex: 0.2,
-        flexDirection: "column",
-
-    },
-    cabecalhoCadastro: {
-        flex: 0.25,
-        flexDirection: "column",
-    },
-
-    fundoCabecalho: {
-        width: "auto", height: 170, 
-    },
-    logo: {
-        width: 250,
-        height: 250,
-        resizeMode: 'contain',
-    },
-
-    sucesso: {
-        width: 130,
-        height: 130,
-        resizeMode: 'contain',
-    },
-    entrada_texto: {
-        height: 60, width: 270, borderWidth: 1, borderColor: '#12B1F5', marginHorizontal: 10, fontSize: 16, padding: 5,
-        borderRadius: 6, marginBottom: 20,
-        textAlign: 'center'
-    },
-    entrada_texto2: {
-        height: 60, width: 270, borderWidth: 1, borderColor: '#12B1F5', marginHorizontal: 10, fontSize: 20, padding: 5, 
-        textAlign: 'center'
-    },
-
-    corpo: {
-        flex: 0.25,
-        paddingHorizontal: 20,
-        alignItems: 'center',
-
-
-    },
-
-    corpoCadastro: {
-        flex: 0.55,
-        paddingHorizontal: 20,
-        alignItems: 'center',
-
-
-    },
-    corpoMenu: {
-        flex: 0.6,
-        paddingHorizontal: 20,
-        justifyContent: "space-between",
-        alignItems: 'center',
-  marginTop: 50
-
-
-    },
-
-    
-    corpoFlatList: {
-        flex: 0.7,
-        paddingHorizontal: 20,
-        alignItems: 'center',
-
-
-    },
-
-    rodapeCadastro: {
-        flex: 0.2,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    rodape: {
-        flex: 0.3,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    rodapeLista: {
-        flex: 0.1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    rodapeBotao: {
-        width: 270,
-        height: 60,
-        backgroundColor: "#12B1F5",
-        borderRadius: 6,
-        justifyContent: 'center',
-        alignItems: 'center',
-
-    },
-    titulo: { color: '#0038A8', textAlign: 'center', fontSize: 'bold', fontSize: 18, marginBottom: 30 },
-
-
-  
-    lista: { marginHorizontal: 20, 
-        backgroundColor: 'white', flex: 0.65, 
-    paddingTop: 30 },
-
- 
-voltar:{width: 50, height: 50, alignItems: 'center', borderRadius: 25},
-
- viewPicker: {
-        flex: 0.48, flexDirection: 'column', alignItems: 'center',
-        width: "90%", paddingLeft: 20, paddingTop: 100
-    },
-    picker: { width: 300, borderRadius: 4, height: 30, margingLeft: 10 },
-
-    exibir: {
-        color: "white", fontWeight: 'bold', borderWidth: 1,
-        borderColor: 'gray', textAlign: 'center', marginTop: 10, width: 100, borderRadius: 20, height: 30, paddingTop: 5, backgroundColor: "#12B1F5"
-    },
-
-    atendimentoHead: {
-        flex: 0.05,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-
-
-    },
-
-    atendimentoHeadText: {
-        fontSize: 18,
-        color: '#0038a8',
-
-    },
-
-    textoConteudo: {
-        color: 'white', 
-        fontSize: 16, 
-        textAlign: 'justify'
-    },
-    separador: {
-        height: 2, backgroundColor: '#12B1F5', marginTop: 4
-    },
-    
-    modalOverlay: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.7)', // Fundo escurecido ao redor da modal
-      },
-      modalContent: {
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
-        alignItems: 'center',
-      },
-      modalTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-      },
-      modalCorrida: {
-        fontSize: 24, // Tamanho maior para o nome do evento
-        fontWeight: 'bold',
-        color: 'black',
-        textAlign: 'center',
-        marginBottom: 20,
-      },
-      modalClose: {
-        color: 'blue',
-        fontSize: 18,
-        marginTop: 10,}, 
-        // Adicione estas linhas no seu arquivo estilos.js
-        cameraIcon: {
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginVertical: 10,
-            marginTop: 20, // Ajuste o valor conforme necessário
-        },
-        
-        
-imagemSelecionada: {
-    width: 100,
-    height: 100,
-    marginTop: 20,
-    resizeMode: 'cover',
-  },
-
-    
-    });
->>>>>>> 7445952cd1ba76e126cb4c5471bc8e60f9c7d606
 export { estilos };
