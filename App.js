8b565a0193f42039efc9d745b62530e58812f539
Vanessa Hillesheim <162062543+vanessahillesheim@users.noreply.github.com>
2024-09-29 19:46:48 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -3,22 +3,18 @@ import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/Navegacao/StackNavigator"; 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
- 
- 
- 
- 
+import { AuthProvider } from './src/servicos/AuthContext';
 function App() {
    return (
      <SafeAreaProvider>
+       <AuthProvider>
          <NavigationContainer>
-            <StatusBar />
-            <StackNavigator />
-         </NavigationContainer>
-         
-      </SafeAreaProvider>      
+           <StatusBar />
+           <StackNavigator />
+         </NavigationContainer> 
+       </AuthProvider>
+     </SafeAreaProvider>      
    );
- 
- 
 }
- 
+
 export default App;
