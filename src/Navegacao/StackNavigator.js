7dd7d0ccdd8d1c602f802267e17187ce15ec6be5
Vanessa Hillesheim <162062543+vanessahillesheim@users.noreply.github.com>
2024-09-29 19:44:12 -0300

    Add files via upload

--- src/Navegacao/StackNavigator.js
+++ src/Navegacao/StackNavigator.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // StackNavigator.js
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,6 +8,9 @@ import CadastroSucessoUser from '../Telas/CadastroSucessoUser'; // Tela de suces
 import CadastroSucessoCorrida from '../Telas/CadastroSucessoCorrida'; // Tela de sucesso do cadastro Corrida
 import DrawerNavigator from './DrawerNavigator'; // Certifique-se de que o caminho está correto
 import CadastroCorrida from '../Telas/CadastroCorrida'; // Tela de cadastro de nova corrida
+import Cabecalho from '../Telas/Cabecalho';
+import AtualizacaoCorrida from '../Telas/AtualizacaoCorrida';
+
 
 const Stack = createStackNavigator();
 
@@ -18,6 +21,9 @@ function StackNavigator() {
       <Stack.Screen name="Tela1" component={Tela1} options={{ headerShown: false }} />
       <Stack.Screen name="CadastroUsuario" component={CadastroUsuario} options={{ headerShown: true }} />
       <Stack.Screen name="CadastroSucessoUser" component={CadastroSucessoUser} options={{ headerShown: false }} />
+      <Stack.Screen name="Cabecalho" component={Cabecalho} options={{ headerShown: false }} />
+      <Stack.Screen name="AtualizacaoCorrida" component={AtualizacaoCorrida} options={{ headerShown: false }} />
+      
       
       {/* O DrawerNavigator será exibido após o login/cadastro bem-sucedido */}
       <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }} />
@@ -30,36 +36,4 @@ function StackNavigator() {
 }
 
 export default StackNavigator;
-=======
-// StackNavigator.js
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import Tela1 from '../Telas/Tela1'; // Tela de login
-import CadastroUsuario from '../Telas/CadastroUsuario'; // Tela de cadastro de usuário
-import CadastroSucessoUser from '../Telas/CadastroSucessoUser'; // Tela de sucesso do cadastro Usuário
-import CadastroSucessoCorrida from '../Telas/CadastroSucessoCorrida'; // Tela de sucesso do cadastro Corrida
-import DrawerNavigator from './DrawerNavigator'; // Certifique-se de que o caminho está correto
-import CadastroCorrida from '../Telas/CadastroCorrida'; // Tela de cadastro de nova corrida
 
-const Stack = createStackNavigator();
-
-function StackNavigator() {
-  return (
-    <Stack.Navigator initialRouteName="Tela1">
-      {/* Telas relacionadas ao login e cadastro */}
-      <Stack.Screen name="Tela1" component={Tela1} options={{ headerShown: false }} />
-      <Stack.Screen name="CadastroUsuario" component={CadastroUsuario} options={{ headerShown: true }} />
-      <Stack.Screen name="CadastroSucessoUser" component={CadastroSucessoUser} options={{ headerShown: false }} />
-      
-      {/* O DrawerNavigator será exibido após o login/cadastro bem-sucedido */}
-      <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }} />
-      
-      {/* Telas de cadastro de corrida */}
-      <Stack.Screen name="CadastroCorrida" component={CadastroCorrida} options={{ headerShown: false }} />
-      <Stack.Screen name="CadastroSucessoCorrida" component={CadastroSucessoCorrida} options={{ headerShown: false }} />
-    </Stack.Navigator>
-  );
-}
-
-export default StackNavigator;
->>>>>>> 7445952cd1ba76e126cb4c5471bc8e60f9c7d606
